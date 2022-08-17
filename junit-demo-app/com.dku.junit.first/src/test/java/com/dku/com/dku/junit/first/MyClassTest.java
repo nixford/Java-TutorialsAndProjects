d5b7d54f5ec625c399b1669d2ab8982f3185126c
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-17 14:41:00 +0300

    add new test for multiply function

--- junit-demo-app/com.dku.junit.first/src/test/java/com/dku/com/dku/junit/first/MyClassTest.java
+++ junit-demo-app/com.dku.junit.first/src/test/java/com/dku/com/dku/junit/first/MyClassTest.java
@@ -14,5 +14,11 @@ class MyClassTest {
 		MyClass tester = new MyClass();
 		assertThrows(IllegalArgumentException.class, () -> tester.multiply(1000, 5));
 	}
+	
+	@Test
+	void testMultiply() {
+		MyClass tester = new MyClass();
+		assertEquals(50, tester.multiply(10, 5), "10 x 5 must be 50");
+	}
 
 }
