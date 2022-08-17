50dcfbb5a5c923c3bb2c3893d9edf999d2a24a48
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-17 14:44:17 +0300

    add example with @BeforeEach test

--- junit-demo-app/com.dku.junit.first/src/test/java/com/dku/com/dku/junit/first/MyClassTest.java
+++ junit-demo-app/com.dku.junit.first/src/test/java/com/dku/com/dku/junit/first/MyClassTest.java
@@ -8,16 +8,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyClassTest {
+	
+	private MyClass tester;
+	
+	@BeforeEach
+	void setup() {
+		tester = new MyClass();
+	}
 
 	@Test
 	void testEsceptionIsThrown() {
-		MyClass tester = new MyClass();
 		assertThrows(IllegalArgumentException.class, () -> tester.multiply(1000, 5));
 	}
 	
 	@Test
 	void testMultiply() {
-		MyClass tester = new MyClass();
 		assertEquals(50, tester.multiply(10, 5), "10 x 5 must be 50");
 	}
 
