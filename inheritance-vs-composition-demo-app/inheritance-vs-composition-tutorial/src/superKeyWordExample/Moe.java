5025f3fa6b6b4e03a362f54aa4b1d2d2d6666036
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-18 16:12:16 +0300

    add comments about super keyword

--- inheritance-vs-composition-demo-app/inheritance-vs-composition-tutorial/src/superKeyWordExample/Moe.java
+++ inheritance-vs-composition-demo-app/inheritance-vs-composition-tutorial/src/superKeyWordExample/Moe.java
@@ -1,17 +1,18 @@
 package superKeyWordExample;
 
 class Moe extends Character {
+	// Accessing constructor
     Moe() {
         super();
     }
     void giveBeer() {
+    	// Accessing method
         super.move();
-        // System.out.println("Give beer");
+        System.out.println("Give beer");
     }
     
     public static void main(String... superExample) {
-    	Moe moe = new Moe();
-    	
+    	Moe moe = new Moe();    	
     	moe.giveBeer();
 	}
 }
