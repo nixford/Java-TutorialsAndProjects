e147516eb85430cd0ff113604be01d7a8d38e6f6
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-18 16:34:19 +0300

    add comments for super invocation if parent has at least one parameter

--- inheritance-vs-composition-demo-app/inheritance-vs-composition-tutorial/src/superContructorFirsLoading/Barney.java
+++ inheritance-vs-composition-demo-app/inheritance-vs-composition-tutorial/src/superContructorFirsLoading/Barney.java
@@ -4,4 +4,13 @@ public class Barney extends Character {
     // No need to declare the constructor or to invoke the super constructor
 	// The super class constructor is invoked first
     // The JVM will to that
+	
+	
+	// If the parent class has a constructor with at least one parameter, 
+    // then we must declare the constructor in the subclass and use 
+    // super to explicitly invoke the parent constructor, for example:
+	
+	//	Barney() {
+	//     super("Barney Gumble");
+	//  }
 } 
--- inheritance-vs-composition-demo-app/inheritance-vs-composition-tutorial/src/superContructorFirsLoading/Character.java
+++ inheritance-vs-composition-demo-app/inheritance-vs-composition-tutorial/src/superContructorFirsLoading/Character.java
@@ -4,4 +4,12 @@ public class Character {
     Character() {
         System.out.println("The super constructor was invoked");
     }
+    
+    // If the parent class has a constructor with at least one parameter, 
+    // then we must declare the constructor in the subclass and use 
+    // super to explicitly invoke the parent constructor, for example
+    
+	//    Character(String name) {
+	//        System.out.println(name + "was invoked");
+	//    }
 }
