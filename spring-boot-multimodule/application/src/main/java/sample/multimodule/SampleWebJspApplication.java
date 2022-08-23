23b8d4f67c6147bbcb37abcb6383b24935b34b27
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-23 18:10:11 +0300

    add auto config tutorial

--- spring-boot-multimodule/application/src/main/java/sample/multimodule/SampleWebJspApplication.java
+++ spring-boot-multimodule/application/src/main/java/sample/multimodule/SampleWebJspApplication.java
@@ -1,7 +1,6 @@
 package sample.multimodule;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
 @SpringBootApplication
 public class SampleWebJspApplication 
 {
