b3c0aefe9328ff9029a459302970ee85d656bbdf
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-23 16:05:11 +0300

    add spring boot multimodule app example

---
+++ spring-boot-multimodule/src/main/java/sample/multimodule/application/SpringBootMultimoduleApplication.java
@@ -0,0 +1,13 @@
+package sample.multimodule.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultimoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultimoduleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-multimodule/src/test/java/sample/multimodule/application/SpringBootMultimoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.multimodule.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultimoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
