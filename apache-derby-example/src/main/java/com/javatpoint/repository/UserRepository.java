3ae4a2c3e320fe369b1554b8f9db36c17eaf51ea
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-24 15:34:14 +0300

    add JPA with ApacheDerby example

---
+++ apache-derby-example/src/main/java/com/javatpoint/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.javatpoint.repository;
+
+import org.springframework.data.repository.CrudRepository;  
+import com.javatpoint.model.UserRecord;  
+
+public interface UserRepository extends CrudRepository<UserRecord, String>   
+{    
+}    
