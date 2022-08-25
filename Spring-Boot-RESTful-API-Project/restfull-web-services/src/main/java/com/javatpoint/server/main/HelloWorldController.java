b513d25c536a3952a945aa94880ed592d9587a3f
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-25 15:48:44 +0300

    add RESTful API init

---
+++ Spring-Boot-RESTful-API-Project/restfull-web-services/src/main/java/com/javatpoint/server/main/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.javatpoint.server.main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+		// using get method and hello-world as URI
+		@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+		public String helloWorld() {
+			return "Hello world!";
+		}
+}
