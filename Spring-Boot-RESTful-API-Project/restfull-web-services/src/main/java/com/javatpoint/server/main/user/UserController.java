4f86c152caf6840b73a7672d266cab416f59bac3
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-25 17:03:54 +0300

    add User and UserController

---
+++ Spring-Boot-RESTful-API-Project/restfull-web-services/src/main/java/com/javatpoint/server/main/user/UserController.java
@@ -0,0 +1,20 @@
+package com.javatpoint.server.main.user;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserDaoService service;
+
+	@GetMapping("/users")
+	public List<User> retriveAllUsers() {
+		return service.findAll();
+	}
+
+}
