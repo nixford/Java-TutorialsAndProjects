3e3b88059b0653a24d5130c4117e7c37d9468a99
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-26 12:53:48 +0300

    add example for extending error class

---
+++ Spring-Boot-RESTful-API-Project/restfull-web-services/src/main/java/com/javatpoint/server/main/exception/ExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.javatpoint.server.main.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+
+	public ExceptionResponse(Date date, String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
