c82fbd2638154416910e8cb233e85b27ed75bd95
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-26 10:20:10 +0300

    add POST method

--- Spring-Boot-RESTful-API-Project/restfull-web-services/src/main/java/com/javatpoint/server/main/user/UserController.java
+++ Spring-Boot-RESTful-API-Project/restfull-web-services/src/main/java/com/javatpoint/server/main/user/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,16 @@ public class UserController {
 	@GetMapping("/users")
 	public List<User> retriveAllUsers() {
 		return service.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
 	}
 
 }
