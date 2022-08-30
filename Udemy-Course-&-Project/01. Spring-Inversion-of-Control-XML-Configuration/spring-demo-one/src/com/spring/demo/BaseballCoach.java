fb8f5d5ed46b6dbbb59c811fb1cf6175afedee17
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-30 17:07:04 +0300

    add coach interface

--- Udemy-Course-&-Project/01. Spring-Inversion-of-Control-XML-Configuration/spring-demo-one/src/com/spring/demo/BaseballCoach.java
+++ Udemy-Course-&-Project/01. Spring-Inversion-of-Control-XML-Configuration/spring-demo-one/src/com/spring/demo/BaseballCoach.java
@@ -1,7 +1,8 @@
 package com.spring.demo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
 	
+	@Override
 	public String getDailyWorkout() {
 		return "BaseballCoach - getDailyWorkout";
 	}
