c3b8418fede19e6c05cb66e39a4c81333dfa5c78
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-08-31 12:56:45 +0300

    add example for dependacy injection with setter

--- Udemy-Course-&-Project/02. Spring-Dependancy-Injection-XML-Configuration/spring-demo-one/src/com/spring/beans/CricketCoach.java
+++ Udemy-Course-&-Project/02. Spring-Dependancy-Injection-XML-Configuration/spring-demo-one/src/com/spring/beans/CricketCoach.java
@@ -6,11 +6,18 @@ import com.spring.interfaces.Fortune;
 public class CricketCoach implements Coach {
 	
 	private String className = getClass().getName();
+	// define a private field for the dependency
 	private Fortune fortuneService;
 	
 	public CricketCoach(Fortune theFortuneService) {
-		fortuneService = theFortuneService;
+		System.out.println("CricketCoach constructor");
 	}
+
+	// EXAMPLE FOR DEPENDENCY INJECTION WITH SETTER
+	public void setFortuneService(Fortune fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 
 	@Override
 	public String getDailyWorkout() {
