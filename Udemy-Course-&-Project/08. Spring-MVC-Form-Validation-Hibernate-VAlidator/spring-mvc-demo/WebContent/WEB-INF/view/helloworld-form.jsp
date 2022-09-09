5390f745af8da27116a65f1f41584993d5f548ce
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2022-09-09 12:23:44 +0300

    add spring-mvc validation

---
+++ Udemy-Course-&-Project/08. Spring-MVC-Form-Validation-Hibernate-VAlidator/spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+	<form action="processForm" method="GET">
+		<input type="text" name="studentName" placeholder="What's your name?" />		
+		<input type="submit" />	
+	</form>
+	<br><br>
+	<form action="processFormVersionTwo" method="GET">
+		<input type="text" name="studentName" placeholder="Example for data transferring with MODEL?" />		
+		<input type="submit" />
+	</form>	
+	<br><br>
+	<form action="processFormVersionThree" method="GET">
+		<input type="text" name="studentName" placeholder="Example for @RequestParam?" />		
+		<input type="submit" />
+	</form>				
+</body>
---
+++ Udemy-Course-&-Project/08. Spring-MVC-Form-Validation-Hibernate-VAlidator/spring-mvc-demo/src/com/luv2code/springdemo/mvc/StudentController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.model.Student;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+	@RequestMapping("/showForm")
+	public String showForm(Model model) {
+
+		Student student = new Student();
+
+		model.addAttribute("student", student);
+
+		return "student-form";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student student) {
+
+		return "student-confirmation";
+	}
+}
